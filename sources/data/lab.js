411a6a834e3a81ad9db265f85efa175212d4c269
迷体星球 <126231149+1299411832@users.noreply.github.com>
2026-09-24 18:37:07 +0800

    Update lab.js

--- sources/data/lab.js
+++ sources/data/lab.js
@@ -1,79 +1,79 @@
 export default [
     {
-        title: 'Black Hole',
-        url: 'https://awwwards-2022-workshop.vercel.app',
+        title: '迷体星球',
+        url: 'https://miti1.cc.cd',
         image: 'black-hole.ktx',
         imageMini: 'black-hole-mini.ktx'
     },
     {
-        title: 'Infinite World',
-        url: 'https://infinite-world.vercel.app',
+        title: '迷影·TV',
+        url: 'https://tv.miti.cc.cd',
         image: 'infinite-world.ktx',
         imageMini: 'infinite-world-mini.ktx'
     },
     {
-        title: 'My Room in 3D',
-        url: 'https://my-room-in-3d.vercel.app',
-        image: 'my-room-in-3d.ktx',
-        imageMini: 'my-room-in-3d-mini.ktx',
-    },
-    {
-        title: 'Particles System',
-        url: 'https://three-js-tsl-particles-system.vercel.app',
-        image: 'particles-system.ktx',
-        imageMini: 'particles-system-mini.ktx'
-    },
-    {
-        title: 'Stylized Low Poly',
-        url: 'https://stylized-low-poly.vercel.app',
-        image: 'stylized-low-poly.ktx',
-        imageMini: 'stylized-low-poly-mini.ktx'
-    },
-    {
-        title: 'Holographic terrain',
-        url: 'https://experiment-holographic-terrain.vercel.app',
-        image: 'holographic-terrain.ktx',
-        imageMini: 'holographic-terrain-mini.ktx'
-    },
-    {
-        title: 'Woodkid Volcano Robot',
-        url: 'https://experiment-woodkid-volcano-robot.vercel.app',
-        image: 'woodkid-volcano-robot.ktx',
-        imageMini: 'woodkid-volcano-robot-mini.ktx'
-    },
-    {
-        title: 'Bounce Friday',
-        url: 'https://threejs-journey.com/apps/bounce-friday/',
-        image: 'bounce-friday.ktx',
-        imageMini: 'bounce-friday-mini.ktx'
-    },
-    {
-        title: 'VFX flames',
-        url: 'https://threejs.org/examples/webgpu_tsl_vfx_flames.html',
-        image: 'vfx-flames.ktx',
-        imageMini: 'vfx-flames-mini.ktx'
-    },
-    {
-        title: 'VFX tornado',
-        url: 'https://threejs.org/examples/webgpu_tsl_vfx_tornado.html',
-        image: 'vfx-tornado.ktx',
-        imageMini: 'vfx-tornado-mini.ktx'
-    },
-    {
-        title: 'DOOM Portal',
-        url: 'https://doom-portal-in-webgl.vercel.app',
+        title: '迷体PLAYER',
+        url: 'https://t.miti.cc.cd',
         image: 'doom-portal.ktx',
         imageMini: 'doom-portal-mini.ktx'
     },
     {
-        title: 'Organic Sphere',
-        url: 'https://organic-sphere.vercel.app',
+        title: '迷体·META',
+        url: 'https://x.miti.cc.cd',
+        image: 'my-room-in-3d.ktx',
+        imageMini: 'my-room-in-3d-mini.ktx',
+    },
+    {
+        title: 'MT·BOX',
+        url: 'https://w.miti.cc.cd',
+        image: 'particles-system.ktx',
+        imageMini: 'particles-system-mini.ktx'
+    },
+    {
+        title: '白嫖·云游戏',
+        url: 'https://cpc.icloud.cn/cloudstatic/inviteReward/?showUserId=12976556&inviteNo=s28ciz&inviteScene=5',
+        image: 'stylized-low-poly.ktx',
+        imageMini: 'stylized-low-poly-mini.ktx'
+    },
+    {
+        title: '迷体星球·折扣店',
+        url: 'https://ficp.fun/s/iZJdfm/?cid=P8GqhZF#/',
+        image: 'holographic-terrain.ktx',
+        imageMini: 'holographic-terrain-mini.ktx'
+    },
+    {
+        title: '迷体星球·淘宝店',
+        url: 'https://shop.m.taobao.com/shop/shop_index.htm?shop_id=112853800',
+        image: 'woodkid-volcano-robot.ktx',
+        imageMini: 'woodkid-volcano-robot-mini.ktx'
+    },
+    {
+        title: '迷体星球·微店',
+        url: 'https://k.youshop10.com/rYmlXkLT',
+        image: 'bounce-friday.ktx',
+        imageMini: 'bounce-friday-mini.ktx'
+    },
+    {
+        title: 'MTAI·设计提示词',
+        url: 'https://ccc.miti.cc.cd/motionsites_prompts_gallery.html',
+        image: 'vfx-flames.ktx',
+        imageMini: 'vfx-flames-mini.ktx'
+    },
+    {
+        title: '全·迷体GAME·街机游戏厅',
+        url: 'https://wgame80.com/category',
+        image: 'vfx-tornado.ktx',
+        imageMini: 'vfx-tornado-mini.ktx'
+    },
+    {
+        title: 'MTGAME',
+        url: 'https://g.miti.cc.cd',
         image: 'organic-sphere.ktx',
         imageMini: 'organic-sphere-mini.ktx'
     },
     {
-        title: 'Attractors',
-        url: 'https://threejs.org/examples/webgpu_tsl_compute_attractors_particles.html',
+        title: '迷体元宇·VR播放器·安卓版',
+        url: 'https://mv.miti.cc.cd',
         image: 'attractors.ktx',
         imageMini: 'attractors-mini.ktx'
     }
