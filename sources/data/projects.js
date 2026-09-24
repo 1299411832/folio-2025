3cbdbaa3d5f578a38aa2a17948c0b593627a3372
迷体星球 <126231149+1299411832@users.noreply.github.com>
2026-09-24 19:14:12 +0800

    Update projects.js

--- sources/data/projects.js
+++ sources/data/projects.js
@@ -1,14 +1,14 @@
 export default [
     {
-        title: 'Three.js Journey',
-        titleSmall: [ 'Three.js', 'Journey' ],
-        url: 'https://threejs-journey.com',
+        title: '迷·链',
+        titleSmall: [ '星沙瓶', '之旅' ],
+        url: 'https://miti1.cc.cd/friends',
         attributes:
         {
-            role: [ 'developer', 'formater' ],
-            with: [ 'Hervé Studio', 'Bonhomme Paris' ]
+            role: [ '虫洞穿梭', '星球漂流' ],
+            with: [ '链接', '随机星球' ]
         },
-        distinctions: [ 'fwa' ], // 'awwwards', 'fwa', 'cssda'
+        distinctions: [ 'mtbox' ], // 'awwwards', 'fwa', 'cssda'
         images:
         [
             'threejs-journey-1.ktx',
@@ -25,7 +25,7 @@ export default [
     //     attributes:
     //     {
     //         at: 'Immersive Garden',
-    //         role: 'WebGL developer',
+    //         role: 'WebGL 开发',
     //         with: 'Rémi Tran'
     //     },
     //     distinctions: [ 'awwwards', 'cssda', 'fwa' ], // 'awwwards', 'fwa', 'cssda'
@@ -37,15 +37,15 @@ export default [
     //     ]
     // },
     {
-        title: 'Bonhomme | 10 ans',
-        titleSmall: [ 'Bonhomme', '10 ans' ],
-        url: 'https://anniversary.bonhommeparis.com',
+        title: '迷·语',
+        titleSmall: [ '树洞星球', '记录生活' ],
+        url: 'https://miti1.cc.cd/moments',
         attributes:
         {
-            role: 'WebGL developer',
-            with: 'Bonhomme Paris'
+            role: '朋友圈',
+            with: '更新相关'
         },
-        distinctions: [ 'awwwards', 'fwa' ],
+        distinctions: [ 'mtbox', 'max' ],
         images:
         [
             'bonhomme-10-ans-1.ktx',
@@ -54,16 +54,16 @@ export default [
         ]
     },
     {
-        title: 'Madbox',
-        titleSmall: [ 'Madbox' ],
-        url: 'https://madbox.io',
+        title: '迷体缝隙',
+        titleSmall: [ '永恒' ],
+        url: 'https://miti1.cc.cd/photowall',
         attributes:
         {
-            role: 'WebGL developer',
-            at: 'Hervé Studio',
-            with: 'Lei Xing'
+            role: '观测生活',
+            at: '照片',
+            with: '永恒瞬间'
         },
-        distinctions: [ 'awwwards' ],
+        distinctions: [ 'photo' ],
         images:
         [
             'madbox-1.ktx',
@@ -72,16 +72,16 @@ export default [
         ]
     },
     {
-        title: 'Luni',
-        titleSmall: [ 'Luni' ],
-        url: 'https://www.luni.app',
+        title: '星·核',
+        titleSmall: [ '文章' ],
+        url: 'https://miti1.cc.cd/timeline',
         attributes:
         {
-            role: 'WebGL developer',
-            at: 'Hervé Studio',
-            with: 'Loïc Belaidremesal'
+            role: '时间',
+            at: '迷体长河',
+            with: '回溯'
         },
-        distinctions: [ 'awwwards', 'fwa' ],
+        distinctions: [ 'timeline', 'start' ],
         images:
         [
             'luni-1.ktx',
@@ -91,15 +91,15 @@ export default [
         ]
     },
     {
-        title: 'Scout',
-        titleSmall: [ 'Scout' ],
-        url: 'https://scout-omega.vercel.app',
+        title: '星球航站',
+        titleSmall: [ '收藏' ],
+        url: 'https://miti1.cc.cd/bookmark',
         attributes:
         {
-            role: 'WebGL developer',
-            at: 'Hervé Studio'
+            role: '航站 开发',
+            at: '导航'
         },
-        distinctions: [  ],
+        distinctions: ['bookmark' ],
         images:
         [
             'scout-1.ktx',
@@ -108,15 +108,15 @@ export default [
         ]
     },
     {
-        title: 'Prior Holdings',
-        titleSmall: [ 'Prio', 'Holdings' ],
-        url: 'https://prior.co.jp/discover/en',
+        title: '折扣星球',
+        titleSmall: [ '优惠券', '折扣' ],
+        url: 'https://ficp.fun/s/iZJdfm/?cid=P8GqhZF#/index/home',
         attributes:
         {
-            role: [ 'Front developer', 'WebGL developer' ],
-            at: 'Immersive Garden'
+            role: [ '网店', '情报' ],
+            at: '全平台线报优惠'
         },
-        distinctions: [ 'awwwards', 'cssda', 'fwa' ],
+        distinctions: [ 'fun', 'lift', 'shop' ],
         images:
         [
             'prior-1.ktx',
@@ -125,15 +125,15 @@ export default [
         ]
     },
     {
-        title: 'Orano',
-        titleSmall:  'Orano',
-        url: 'https://www.orano.group/experience/innovation/en',
+        title: '虫洞',
+        titleSmall:  '留言',
+        url: 'https://miti1.cc.cd/messages',
         attributes:
         {
-            role: [ 'Front developer', 'WebGL developer' ],
-            at: 'Immersive Garden'
+            role: [ '树洞', '小心思' ],
+            at: '社区交流'
         },
-        distinctions: [ 'awwwards', 'cssda', 'fwa' ],
+        distinctions: [ 'messages'],
         images:
         [
             'orano-1.ktx',
@@ -142,16 +142,16 @@ export default [
         ]
     },
     {
-        title: 'Citrix / Redbull',
-        titleSmall: ['Citrix', 'Redbull'],
-        url: 'https://thenewmobileworkforce.imm-g-prod.com',
+        title: '迷体星球',
+        titleSmall: ['主页', '工具'],
+        url: 'https://miti1.cc.cd',
         attributes:
         {
-            role: [ 'WebGL developer' ],
-            at: 'Immersive Garden',
-            with: [ 'Damien Doussaud', 'Quentin Leclercq' ]
+            role: [ '应用生态' ],
+            at: '集合体',
+            with: [ '工具箱', '学习与体验' ]
         },
-        distinctions: [ 'awwwards', 'cssda', 'fwa' ],
+        distinctions: [ 'home', 'play', 'work' ],
         images:
         [
             'citrix-redbull-1.ktx',
