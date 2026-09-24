7a3f5c6138d5f5c06b7141dc1bb5eeaf4136b11d
迷体星球 <126231149+1299411832@users.noreply.github.com>
2026-09-24 19:26:02 +0800

    Replace social links with new entries

--- sources/data/social.js
+++ sources/data/social.js
@@ -1,10 +1,10 @@
 export default [
-    { name: 'X', url: 'https://x.com/bruno_simon', align: 'right' },
-    { name: 'Bluesky', url: 'https://bsky.app/profile/bruno-simon.bsky.social', align: 'right' },
-    { name: 'Youtube', url: 'https://www.youtube.com/@BrunoSimon', align: 'right' },
-    { name: 'Mail', url: 'mailto:simon.bruno.77@gmail.com', align: 'right' },
-    { name: 'Twitch', url: 'https://www.twitch.tv/bruno_simon_dev', align: 'right' },
-    { name: 'GitHub', url: 'https://github.com/brunosimon', align: 'right' },
-    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/simonbruno77/', align: 'left' },
+    { name: '迷·链', url: 'https://miti1.cc.cd/friends', align: 'right' },
+    { name: '迷·语', url: 'https://miti1.cc.cd/moments', align: 'right' },
+    { name: '迷体缝隙', url: 'https://miti1.cc.cd/photowall', align: 'right' },
+    { name: '邮箱', url: 'mailto:1299411832@qq.com', align: 'right' },
+    { name: '星·核', url: 'https://miti1.cc.cd/timeline', align: 'right' },
+    { name: '折扣星球', url: 'https://ficp.fun/s/iZJdfm/?cid=P8GqhZF#/index/home', align: 'right' },
+    { name: '迷体PLAYER', url: 'https://t.miti.cc.cd', align: 'left' },
     { name: 'Discord', modal: 'discord', align: 'left' },
 ]
